3f74342334a68033e9403cb1b7c1ec6ff49f1072
johannady2 <johannady2@gmail.com>
2021-03-12 00:09:07 +0900

    issue: duplicates

--- script.js
+++ script.js
@@ -5,31 +5,80 @@
 // Bonus: Make it so it organizes strings differently from number types.
 // i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
 
+//useful tutorial
 //https://www.youtube.com/watch?v=MWD-iKzR2c8&list=PLUp2SeJP6HNB4UmTFyAbvmiVmfi0tpWlH&index=6
 
-let originalArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
-
-let arrayOfObjects =
-[
-    {x:2,y:10},
-    {x:5,y:6}
-]
+let inputArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+let cloneOriginalArray = inputArray;
 
 function organizeArray(arr)
 {
     return arr.sort();//also sorts alphabetically. sorts numbers first, then capital letters first, then lower case letters
 }
 
-let organizedArray = organizeArray(originalArray);
 
-function compare(a,b)
+function groupSimilarValues(arr,refP,currentStateOfFinalArray)
 {
-    return a.y-b.y;//y is a parameter of objects in arrayOfObjects (smallest to highest)
-    //return b.y-a.y;//
+    let startHere = 0;
+    let referencePoint = refP;
+    let currentState = currentStateOfFinalArray; //when recurssion of the function occurs, the last state is passed to this parameter.
+    let tempArray = []; //alwaysEmpty when function starts
+
+    for(startHere=0;startHere<arr.length;startHere++)
+    {
+        
+        //console.log(`${arr[referencePoint]} and ${arr[startHere]}`);
+        if(arr[referencePoint] === arr[startHere])
+        {
+            tempArray.push(arr[startHere]);
+        }
+
+    }
+
+    
+
+    if(referencePoint < (arr.length))
+    {
+
+
+        console.log(`before splice: ${currentState}`);
+      
+        //currentState.splice(currentState.length,0, ...tempArray);//always splices at the end of the array
+        if(tempArray.length > 1)
+        {
+            currentState = currentState.concat([tempArray],undefined);
+           
+        }
+        else
+        {
+            currentState = currentState.concat(tempArray[0],undefined);
+            
+        }
+        console.log(`after splice: ${currentState}`);
+        referencePoint++;
+
+        console.log(`new value of reference point is ${referencePoint}`);
+      
+     
+        groupSimilarValues(arr,referencePoint,currentState);
+    }
+    else
+    {
+        currentState = removeUndefinedFromArray(currentState);
+        console.log(currentState);
+        return currentState;
+    }
 }
 
-console.log(organizedArray);//(hightest to smallest)
-console.log(originalArray);//NOTE original array is also changed because sort() is a destructive function.
+function removeUndefinedFromArray(arrayWithUndefineds)
+{
+    let filtered = arrayWithUndefineds.filter(function (el) {
+        return el != null;
+      });
+      
+      return filtered;
+}
 
-arrayOfObjects.sort(compare);
-console.log(arrayOfObjects);
+let sortedArray =  organizeArray(inputArray);
+console.log(sortedArray);
+groupSimilarValues(sortedArray,0,[]);
